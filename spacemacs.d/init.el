78771eadb9874700aad4f6458872064f8fac808a
Colin.Lee <geek.colin@gmail.com>
2016-12-26 17:28:15 +0800

    first commit

---
+++ spacemacs.d/init.el
@@ -0,0 +1,189 @@
+;; -*- mode: emacs-lisp -*-
+;; brew tap railwaycat/emacsmacport
+;; brew install emacs-mac --with-spacemacs-icon --HEAD --with-gnutls
+(defun dotspacemacs/layers ()
+  (setq-default
+   dotspacemacs-distribution 'spacemacs
+   dotspacemacs-enable-lazy-installation 'nil
+   dotspacemacs-ask-for-lazy-installation t
+   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layers
+   '(
+     yaml
+     ivy
+     better-defaults
+     python
+     html
+     emacs-lisp
+     git
+     docker
+     markdown
+     osx
+     ranger
+     shell-scripts
+     vimscript
+     search-engine
+     docker
+     (chinese :variables chinese-enable-youdao-dict t)
+     (org :variables org-enable-reveal-js-support t)
+     (shell :variables
+            shell-default-shell 'eshell
+            shell-default-height 60
+            shell-default-term-shell "zsh")
+     (syntax-checking :variables
+                      syntax-checking-enable-by-default t
+                      syntax-checking-enable-tooltips t
+                      )
+     (auto-completion :variables
+                      auto-completion-enable-sort-by-usage t
+                      auto-completion-enable-help-tooltip t
+                      auto-completion-enable-snippets-in-popup t
+                      auto-completion-private-snippets-directory "~/.spacemacs.d/snippets/"
+                      )
+     (colors :variables colors-enable-nyan-cat-progress-bar t)
+     (ibuffer :variables ibuffer-group-buffers-by 'projects))
+
+   dotspacemacs-additional-packages'()
+   dotspacemacs-frozen-packages '()
+   dotspacemacs-excluded-packages '(
+                                    ido, tern, vi-tilde-fringe)
+
+   dotspacemacs-install-packages 'used-only))
+
+(defun dotspacemacs/init ()
+  (setq-default
+   dotspacemacs-elpa-https t
+   dotspacemacs-elpa-timeout 10
+   dotspacemacs-check-for-update nil
+   dotspacemacs-elpa-subdirectory nil
+   dotspacemacs-editing-style 'vim
+   dotspacemacs-verbose-loading nil
+   dotspacemacs-startup-banner 'official
+   ;; Possible values for list-type are:
+   ;; `recents' `bookmarks' `projects' `agenda' `todos'."
+   dotspacemacs-startup-lists '((recents . 5)
+                                (projects . 7))
+   dotspacemacs-startup-buffer-responsive t
+   dotspacemacs-scratch-mode 'text-mode
+   dotspacemacs-themes '(spacemacs-dark monokai dracula )
+   dotspacemacs-colorize-cursor-according-to-state t
+   dotspacemacs-default-font '("Source Code Pro"
+                               :size 15
+                               :weight normal
+                               :width normal
+                               :powerline-scale 1.1)
+   dotspacemacs-leader-key "SPC"
+   dotspacemacs-emacs-leader-key "M-m"
+   dotspacemacs-major-mode-leader-key ","
+   dotspacemacs-major-mode-emacs-leader-key "C-M-m"
+   dotspacemacs-emacs-command-key "SPC"
+   dotspacemacs-distinguish-gui-tab nil
+   dotspacemacs-remap-Y-to-y$ t
+   dotspacemacs-retain-visual-state-on-shift t
+   dotspacemacs-visual-line-move-text t
+   dotspacemacs-ex-substitute-global nil
+   dotspacemacs-default-layout-name "Default"
+   dotspacemacs-display-default-layout nil
+   dotspacemacs-auto-resume-layouts nil
+   dotspacemacs-large-file-size 1
+   dotspacemacs-auto-save-file-location 'cache
+   dotspacemacs-max-rollback-slots 5
+   dotspacemacs-helm-resize nil
+   dotspacemacs-helm-no-header nil
+   dotspacemacs-helm-position 'bottom
+   dotspacemacs-helm-use-fuzzy 'always
+   dotspacemacs-enable-paste-transient-state nil
+   dotspacemacs-which-key-delay 0.1
+   dotspacemacs-which-key-position 'bottom
+   dotspacemacs-loading-progress-bar t
+   dotspacemacs-fullscreen-at-startup nil
+   dotspacemacs-fullscreen-use-non-native nil
+   dotspacemacs-maximized-at-startup t
+   dotspacemacs-active-transparency 90
+   dotspacemacs-inactive-transparency 90
+   dotspacemacs-show-transient-state-title t
+   dotspacemacs-show-transient-state-color-guide t
+   dotspacemacs-mode-line-unicode-symbols t
+   dotspacemacs-smooth-scrolling t
+   dotspacemacs-line-numbers 'relative
+   dotspacemacs-folding-method 'evil
+   dotspacemacs-smartparens-strict-mode t
+   dotspacemacs-smart-closing-parenthesis nil
+   dotspacemacs-highlight-delimiters 'all
+   dotspacemacs-persistent-server nil
+   dotspacemacs-search-tools '("ag" "grep" "pt" "ack")
+   dotspacemacs-default-package-repository nil
+   dotspacemacs-whitespace-cleanup nil
+   ))
+
+(defun dotspacemacs/user-init ()
+  (setq configuration-layer--elpa-archives
+    '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+      ("org-cn"   . "http://elpa.emacs-china.org/org/")
+      ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+
+  (setq tramp-ssh-controlmaster-options
+        "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+
+  (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
+  (load custom-file 'no-error 'no-message)
+  (setq exec-path-from-shell-check-startup-files nil)
+
+  )
+
+(defun dotspacemacs/user-config ()
+  (setcdr evil-insert-state-map nil)
+  (define-key evil-insert-state-map [escape] 'evil-normal-state)
+  (setq powerline-default-separator 'brace)
+
+  (setq byte-compile-warnings '(not obsolete))
+  (setq warning-minimum-level :error)
+  (fset 'evil-visual-update-x-selection 'ignore)
+
+  ;; key bindings  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; (define-key evil-insert-state-map  (kbd "C-j C-j") 'evil-change-to-previous-state)
+  (spacemacs/declare-prefix "o" "owner")
+
+  ;; youdao Dict
+  (global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point+)
+  (spacemacs/set-leader-keys "oy" 'youdao-dictionary-search-at-point+)
+  ;; (,) add Space
+  (global-set-key (kbd ",") #'(lambda () (interactive) (insert ", ")))
+
+  ;; prog-mode config ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (add-hook 'prog-mode-hook 'which-function-mode)
+  (add-hook 'prog-mode-hook 'hungry-delete-mode)
+  ;; format header line
+  ;; (setq-default header-line-format
+  ;;               '((which-func-mode ("" which-func-format " "))))
+
+  ;; auto complete config  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; abbrev
+  (add-hook 'after-init-hook 'global-company-mode)
+  (define-abbrev-table 'global-abbrev-table '(
+                        ("8mail" "geek.colin@gmail.com")
+                        ("8name" "Colin.Lee")))
+  ;; run shell command
+  (defun shell-command-on-buffer ()
+    (interactive)
+    (shell-command-on-region
+     (point-min) (point-max)
+     (read-shell-command "Run shell command on buffer: ")))
+
+  (spacemacs/set-leader-keys "or" 'shell-command-on-buffer)
+
+  ;; org-mode config ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; org-mode auto new line
+  (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+  (add-hook 'org-mode-hook 'org-indent-mode)
+  ;; org to el
+  ;; (org-babel-load-file (expand-file-name "colin.org" user-emacs-directory))
+  (eval-after-load "org"
+    '(require 'ox-md nil t))
+
+  ;; global setting ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (delete-selection-mode 1)
+  (setq which-key-side-window-max-height 0.8)
+  (setq which-key-side-window-max-width 0.1)
+
+  )
---
+++ zshrc
@@ -0,0 +1,130 @@
+# Path to your oh-my-zsh installation.
+export ZSH=/Users/colin/.oh-my-zsh
+
+# ZSH_THEME="bhilburn/powerlevel9k"
+# ZSH_THEME="apple"
+
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Uncomment the following line to use hyphen-insensitive completion. Case
+# sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment the following line to disable bi-weekly auto-update checks.
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment the following line to change how often to auto-update (in days).
+export UPDATE_ZSH_DAYS=7
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+ # Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+HIST_STAMPS="mm/dd/yyyy"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=$ZSH/custom
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+# plugins=(git virtualenv docker autojump colored-man-pages)
+
+### User configuration
+source $ZSH/oh-my-zsh.sh
+export LANG=en_US.UTF-8
+export EDITOR='vim'
+export CLICOLOR=1
+export LSCOLORS=Fxbxaxdxcxegedabagacad
+TERM=xterm-256color
+# enable ctrl-s
+stty -ixon
+
+### alias settings
+alias v="mvim -f"
+alias vim="mvim -v"
+alias vi="mvim -v"
+alias pc="proxychains4"
+alias e="emacs -nw"
+alias set_http_proxy="export http_proxy=http://localhost:8087 https_proxy=http://localhost:8087"
+alias set_socks_proxy="export http_proxy=socks5://localhost:1080 https_proxy=socks5://localhost:1080"
+alias unset_proxy="unset http_proxy https_proxy"
+
+### Plugin lists
+## check antigen 
+if [ !  -f ~/.antigen/antigen.zsh ]; then
+    echo "antigen not found, bootstrapping."
+    mkdir -p ~/.antigen
+    curl -SsL  -o ~/.antigen/antigen.zsh https://raw.githubusercontent.com/zsh-users/antigen/master/bin/antigen.zsh
+fi
+source ~/.antigen/antigen.zsh
+
+antigen theme bhilburn/powerlevel9k powerlevel9k
+
+antigen bundles <<EOBUNDLES
+  git
+  osx
+  autojump
+  virtualenv
+  docker
+  docker-compose
+  colored-man-pages
+  zsh-users/zsh-autosuggestions
+  zsh-users/zsh-syntax-highlighting
+
+EOBUNDLES
+
+### Plugin Configuration
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=8
+
+POWERLEVEL9K_MODE='awesome-patched'
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=4
+POWERLEVEL9K_SHORTEN_DELIMITER=".."
+POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
+POWERLEVEL9K_MULTILINE_SECOND_PROMPT_PREFIX="%{%B%F{yellow}%K{blue}%}CMD%{%b%f%k%F{blue}%} %{%f%}"
+POWERLEVEL9K_TIME_FORMAT="%D{%D %T}"
+POWERLEVEL9K_SHOW_CHANGESET=false
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon context dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(background_jobs virtualenv time history status)
+
+# apply plugin and theme
+antigen apply
+
+# ======= percol ======
+# configfile: ~/.percol.d/rc.py
+function exists { which $1 &> /dev/null }
+
+exists percol || pip install percol
+
+if exists percol; then
+    function percol_select_history() {
+        local tac
+        exists gtac && tac="gtac" || { exists tac && tac="tac" || { tac="tail -r" } }
+        BUFFER=$(fc -l -n 1 | eval $tac | percol --query "$LBUFFER")
+        CURSOR=$#BUFFER         # move cursor
+        zle -R -c               # refresh
+    }
+
+    zle -N percol_select_history
+    bindkey '^R' percol_select_history
+fi
